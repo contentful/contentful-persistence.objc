dc9651c7b28c947552f91d1d56a181176afb0856
Boris Bügling <boris@icculus.org>
2014-12-11 12:39:29 +0100

    Clean-up

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  contentful-persistence.objcTests.m
-//  contentful-persistence.objcTests
-//
-//  Created by Boris Bügling on 12/11/2014.
-//  Copyright (c) 2014 Boris Bügling. All rights reserved.
-//
-
-${TEST_EXAMPLE}
